5f1b6fd52c6909b74897977944417718f7cd644b
Brunx96 <160648683+Brunx96@users.noreply.github.com>
2024-03-14 19:47:33 +0000

    Programação

---
+++ operadores-aritmeticos.js
@@ -0,0 +1,16 @@
+const numero1 = 10;
+const numero2 = 3.6;
+
+let soma = numero1 + numero2;
+let subtracao = numero1 - numero2;
+let multiplicacao = numero1 * numero2;
+let divisao = numero1 / numero2;
+let resto = numero1 % numero2;
+
+console.log(`Os números são: ${numero1} ${numero2}`);
+console.log(`A soma dos números é: ${numero1 + numero2}`);
+console.log(`A subtracao dos números é: ${numero1 - numero2}`);
+console.log(`A multiplicacao dos números é: ${numero1 * numero2}`);
+console.log(`A divisao dos números é: ${numero1 / numero2}`);
+console.log(`O resto dos números é: ${numero1 % numero2}`);
+console.log(`A potência do numero1 ao numero2 é: ${numero1**numero2}`)
---
+++ operadores-comparacao.js
@@ -0,0 +1,19 @@
+const numero1 = 50;
+const numero2 = 60;
+const condicao = numero1 != numero2;
+
+if (condicao) {
+    console.log(`A condição é verdadeira`);
+}
+else {
+    console.log(`A condição é falsa`);
+}
+
+// == -> igual
+// != -> não é igual
+// > -> Maior que 
+// < -> Menor que 
+// >= -> Maior OU igual
+// <= -> Menor OU igual
+// === -> Estritamente igual
+// !== -> não estritamente igual
